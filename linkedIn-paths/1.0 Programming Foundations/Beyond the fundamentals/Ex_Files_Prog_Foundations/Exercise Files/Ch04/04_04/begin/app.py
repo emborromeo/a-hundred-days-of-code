7ef77cdbac150340a6be78093664e8c39276d5cd
emborromeo <ericaborromeo06@gmail.com>
2021-08-17 16:52:42 +0800

    added notes

--- linkedIn-paths/1.0 Programming Foundations/Beyond the fundamentals/Ex_Files_Prog_Foundations/Exercise Files/Ch04/04_04/begin/app.py
+++ linkedIn-paths/1.0 Programming Foundations/Beyond the fundamentals/Ex_Files_Prog_Foundations/Exercise Files/Ch04/04_04/begin/app.py
@@ -1,2 +1,4 @@
 miles = input('Enter a distance in miles: ')
-# kilometers_value = miles_value * 1.609344
+miles_value=float(miles)
+kilometers_value = miles_value * 1.609344
+print(kilometers_value)
